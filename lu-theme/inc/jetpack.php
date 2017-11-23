b37994575652ca44cd662ec390cd49a2df072a1f
Luis Amador <mrluisamador@gmail.com>
2017-11-23 15:02:47 -0500

    update the inc folder

--- lu-theme/inc/jetpack.php
+++ lu-theme/inc/jetpack.php
@@ -1,22 +1,42 @@
 <?php
 /**
  * Jetpack Compatibility File
- * See: https://jetpack.me/
+ *
+ * @link https://jetpack.com/
  *
  * @package la
  */
 
 /**
- * Add theme support for Infinite Scroll.
- * See: https://jetpack.me/support/infinite-scroll/
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
  */
 function la_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'render'    => 'la_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
-} // end function la_jetpack_setup
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => 'la-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+	) );
+}
 add_action( 'after_setup_theme', 'la_jetpack_setup' );
 
 /**
@@ -25,6 +45,10 @@ add_action( 'after_setup_theme', 'la_jetpack_setup' );
 function la_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
 	}
-} // end function la_infinite_scroll_render
+}
